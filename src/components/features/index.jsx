882ff142698d5727a95547027c909a1df99202a9
Nayeem <nayeemuddin-bd100@gmail.com>
2021-03-06 21:36:34 +0600

    features page divided to another component

--- src/components/features/index.jsx
+++ src/components/features/index.jsx
@@ -1,4 +1,5 @@
 import img2 from "../../assets/imgs/img-2.png";
+import Item from "./item";
 
 function Features() {
     return (
@@ -9,7 +10,8 @@ function Features() {
             <div className="col-lg-8 col-xl-7 mx-auto text-center mb-5">
               <p className="block__pre-title mb-2">WHY CHOOSING US</p>
               <h1 className="block__title mb-3">
-                Easy Process With <span className="highlight">Best Features</span>
+                Easy Process With{" "}
+                <span className="highlight">Best Features</span>
               </h1>
               <p className="block__paragraph mb-0">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -19,51 +21,26 @@ function Features() {
             <div className="row align-items-center flex-column-reverse flex-lg-row px-2">
               {/* <!-- LEFT CONTENT --> */}
               <div className="col-lg-4">
-                <div className="card-2 d-flex flex-row flex-lg-row-reverse">
-                  <div>
-                    <span className="card-2__symbol mx-auto">
-                      <i className="fas fa-heartbeat"></i>
-                    </span>
-                  </div>
-                  <div className="px-2"></div>
-                  <div>
-                    <h3 className="card-2__title mb-2">Heart Rate Monitor</h3>
-                    <p className="card-2__paragraph">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry.
-                    </p>
-                  </div>
-                </div>
-                <div className="card-2 d-flex flex-row flex-lg-row-reverse">
-                  <div>
-                    <span className="card-2__symbol mx-auto">
-                      <i className="fas fa-briefcase-medical"></i>
-                    </span>
-                  </div>
-                  <div className="px-2"></div>
-                  <div>
-                    <h3 className="card-2__title mb-2">Blood Pressure Manager</h3>
-                    <p className="card-2__paragraph">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry.
-                    </p>
-                  </div>
-                </div>
-                <div className="card-2 d-flex flex-row flex-lg-row-reverse">
-                  <div>
-                    <span className="card-2__symbol mx-auto">
-                      <i className="fas fa-bell"></i>
-                    </span>
-                  </div>
-                  <div className="px-2"></div>
-                  <div>
-                    <h3 className="card-2__title mb-2">Alarm Alert</h3>
-                    <p className="card-2__paragraph">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry.
-                    </p>
-                  </div>
-                </div>
+                <Item
+                  icon="fas fa-heartbeat"
+                  tittle="Heart Rate Monitor"
+                  info="Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry."
+                ></Item>
+
+                <Item
+                  icon="fas fa-briefcase-medical"
+                  tittle="Blood Pressure Manager"
+                  info="Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry."
+                ></Item>
+
+                <Item
+                  icon="fas fa-bell"
+                  tittle="Alarm Alert"
+                  info="Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry."
+                ></Item>
               </div>
               {/* <!-- IMAGE --> */}
               <div className="col-lg-4 my-5 text-center">
@@ -71,51 +48,26 @@ function Features() {
               </div>
               {/* <!-- RIGHT CONTENT --> */}
               <div className="col-lg-4">
-                <div className="card-2 d-flex">
-                  <div>
-                    <span className="card-2__symbol mx-auto">
-                      <i className="fas fa-map-marker-alt"></i>
-                    </span>
-                  </div>
-                  <div className="px-2"></div>
-                  <div>
-                    <h3 className="card-2__title mb-2">Location Finder</h3>
-                    <p className="card-2__paragraph">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry.
-                    </p>
-                  </div>
-                </div>
-                <div className="card-2 d-flex">
-                  <div>
-                    <span className="card-2__symbol mx-auto">
-                      <i className="fas fa-fingerprint"></i>
-                    </span>
-                  </div>
-                  <div className="px-2"></div>
-                  <div>
-                    <h3 className="card-2__title mb-2">Fingerprint Lock</h3>
-                    <p className="card-2__paragraph">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry.
-                    </p>
-                  </div>
-                </div>
-                <div className="card-2 d-flex">
-                  <div>
-                    <span className="card-2__symbol mx-auto">
-                      <i className="fas fa-mobile"></i>
-                    </span>
-                  </div>
-                  <div className="px-2"></div>
-                  <div>
-                    <h3 className="card-2__title mb-2">Camera and Bluetooth</h3>
-                    <p className="card-2__paragraph">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry.
-                    </p>
-                  </div>
-                </div>
+                <Item
+                  icon="fas fa-map-marker-alt"
+                  tittle="Location Finder"
+                  info="Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry."
+                ></Item>
+               
+
+                <Item
+                  icon="fas fa-fingerprint"
+                  tittle="Fingerprint Lock"
+                  info="Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry."
+                ></Item>
+                <Item
+                  icon="fas fa-mobile"
+                  tittle="Camera and Bluetooth"
+                  info="Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry."
+                ></Item>
               </div>
             </div>
           </div>
